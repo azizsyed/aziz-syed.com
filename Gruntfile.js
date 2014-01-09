0e662ec82002b719191cce3c39d5faa2925f743f
Aziz Syed <azizs@lt-mbp-nyc-237.ny.rga.com>
2014-01-09 00:06:39 -0800

    Version 4 commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,165 +1,126 @@
 module.exports = function (grunt) {
-    var loadNpmTasks = function () {
-        grunt.loadNpmTasks('grunt-compass');
-        grunt.loadNpmTasks('grunt-contrib-jshint');
-        grunt.loadNpmTasks('grunt-contrib-watch');
-        grunt.loadNpmTasks('grunt-contrib-concat');
-        grunt.loadNpmTasks('grunt-contrib-jasmine');
-        grunt.loadNpmTasks('grunt-bower-task');
-    };
+  var loadNpmTasks = function () {
+  };
 
-    // Project configuration.
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        meta: {
-            banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' + '<%= grunt.template.today("yyyy-mm-dd") %>\n' + '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' + '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' + ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    meta: {
+      banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' + '<%= grunt.template.today("yyyy-mm-dd") %>\n' + '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' + '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' + ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
+    },
+    watch: {
+      jshint: {
+        files: '<%= jshint.all %>',
+        tasks: ['jshint'],
+        options: {}
+      },
+      styles: {
+        files: '<%= compass.dev.options.sassDir %>/**/*.scss',
+        tasks: ['compass:dev']
+      },
+      templates: {
+        files: 'workspace/templates/**/*.*',
+        tasks: ['assemble']
+      }
+    },
+    bower: {
+      install: {
+        options: {
+          targetDir: './',
+          cleanup: false,
+          install: true
+        }
+      }
+    },
+    compass: {
+      options: {
+        cssDir: 'dist/assets/styles',
+        environment: 'development',
+        force: true
+      },
+      dev: {
+        options: {
+          sassDir: 'workspace/scss/project'
+        }
+      },
+      fontawesome: {
+        options: {
+          sassDir: ['components/font-awesome/scss']
+        }
+      },
+      bootstrap: {
+        options: {
+          sassDir: ['components/bootstrap-sass/lib']
+        }
+      }
+    },
+    jshint: {
+      all: ['Gruntfile.js'],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 9001,
+          base: 'dist'
+        }
+      }
+    },
+    copy: {
+      bootstrap: {
+        cwd: 'components/bootstrap-sass/fonts',
+        src: '**',
+        expand: true,
+        dest: 'dist/assets/fonts/'
+      },
+      fontawesome: {
+        cwd: 'components/font-awesome/fonts',
+        src: '**',
+        expand: true,
+        dest: 'dist/assets/fonts/'
+      }      
+    },
+    clean: {
+      dist: ['dist']
+    },
+    assemble: {
+      options: {
+        prettify: {
+          indent: 2
         },
-        watch: {
-            jshint: {
-                files: '<%= jshint.all %>',
-                tasks: ['jshint'],
-                options: {}
-            },
-            'compass-dev': {
-                files: '<%= compass.dev.src %>/**/*.scss',
-                tasks: ['compass:dev']
-            },
-            'compass-lib': {
-                files: '<%= compass.lib.src %>/**/*.scss',
-                tasks: ['compass:lib']
-            },
-            'templates': {
-                files: 'workspace/templates/**/*.*',
-                tasks: ['templates']
-            }
+        assets: 'dist/assets',
+        partials: ['workspace/templates/partials/**/*.hbs'],
+        helpers: ['helper-moment','templates/helpers/helper-*.js'],
+        layout: 'default.hbs',
+        layoutdir: 'workspace/templates/layouts',
+        data: ['workspace/templates/data/*.{json,yml}']
+      },
+      pages: { expand: true, cwd: 'workspace/templates/pages', src: ['**/*.hbs'], dest: 'dist' }
+    },
+    'ftp-deploy': {
+      build: {
+        auth: {
+          host: 'ftp.azizsyed.com',
+          port: 21,
+          authKey: 'key1'
         },
-        bower: {
-            install: {
-                options: {
-                    targetDir: './',
-                    cleanup: false,
-                    install: true
-                }
-            }
-        },
-        compass: {
-            dev: {
-                src: 'workspace/scss/project',
-                dest: 'deploy/assets/css',
-                outputstyle: 'expanded',
-                linecomments: true
-            },
-            lib: {
-                src: 'workspace/scss/lib',
-                dest: 'deploy/assets/lib',
-                outputstyle: 'expanded',
-                linecomments: true
-            }
-        },
-        jshint: {
-            all: ['Gruntfile.js', 'deploy/assets/scripts/*.js', 'deploy/assets/scripts/modules/**/*.js'],
-            options: {
-                jshintrc: '.jshintrc'
-            }
-        },
-        templates: {
-            dev: {
-                options: {
-                    engine: "handlebars",
-                    language: "en-us",
-                    flatten: false,
-                    production: false,
-                    layout: 'workspace/templates/layout.mustache',
-                    partials: 'workspace/templates/partials/**/*.mustache',
-                    data: ['workspace/templates/data/**/*.json']
-                },
-                files: {
-                    'deploy/': ['workspace/templates/pages/**/*.mustache']
-                }
-            }
-        },
-        jasmine: {
-            src: 'deploy/assets/scripts/modules/*.js',
-            options: {
-                specs: 'workspace/test/jasmine/specs/**/*.js',
-                vendor: ['deploy/assets/scripts/lib/jquery/*.js'],
-                junit: {
-                    path: 'workspace/test/jasmine/junit',
-                    consolidate: true
-                }
-            }
-        },
-        typescript: {
-            compile: {
-                files: {
-                    'deploy/assets/scripts': ['workspace/scripts/*.ts', 'workspace/scripts/modules/**/*.ts']
-                },
-                options: {
-                    baseOutputPath: 'workspace/scripts'
-                }
-            },
-            options: {
-                //basePath: 'test'
-                comments: false,
-                noresolve: false
-            }
-        },
-        casper: {
-            dist: {
-                src: ['workspace/test/casper/*.js'],
+        src: 'dist',
+        dest: '',
+        exclusions: []
+      }
+    }
+  });
 
-                // CasperJS test command options
-                options: {
-                    // Allows you to pass variables to casper that can be accesed in files,
-                    // for example, if you used the following args object then
-                    // casper.cli.get('username') would return 'colin'
-                    /*
-                    */
-                    args: {
-                        //username: 'colin'
-                        basePath: 'workspace/test/casper/'
-                    },
-                    // Exports results of test to a xUnit XML file
-                    //xunit: 'workspace/casper/test/xunit/userSuite.xml',
-                    // Outputs additional log messages
-                    direct: true,
-                    // Sets logging level, check out http://casperjs.org/logging.html
-                    logLevel: 'info',
-                    // Specifies files to be included for each test file
-                    /*
-                    includes: [
-                        'tests/config.js',
-                        'lib/jquery.min.js'],
-                    // Adds tests from specified files before running the test suite
-                    pre: ['tests/pre-test.js'],
-                    // Adds tests from specified files after running the test suite
-                    post: ['tests/post-test.js'],
-                    */
-                    // Terminates test suite upon failure of first test
-                    failFast: false,
+  //Load plugins
+  require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+  grunt.loadNpmTasks("assemble");
 
-                    // grunt-ghost specific options
-                    // Prints the command given to CasperJS
-                    printCommand: false,
+  grunt.registerTask('build', ['jshint', 'clean', 'compass', 'copy', 'assemble']);
 
-                    // Prints list of filepaths
-                    printFilePaths: true
-                }
-            }
-        },
-        uglify: {}
-    });
+  // Default task.
+  grunt.registerTask('default', ['build']);
 
-    loadNpmTasks();
-
-    //Load task config files; from the 'tasks' subfolder
-    grunt.loadTasks('build/tasks');
-
-    grunt.registerTask('lint', ['jshint']);
-    grunt.registerTask('build', ['bower:install', 'compass', 'templates', 'typescript']);
-
-    // Default task.
-    grunt.registerTask('default', ['lint', 'compass:dev', 'templates']);
-    grunt.registerTask('test', ['jasmine']);
-    grunt.registerTask('test-full', ['jasmine', 'casper']);
+  grunt.registerTask('deploy', ['build', 'ftp-deploy']);
 };
