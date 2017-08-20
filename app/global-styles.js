c18988b017a19ce2c9ce1e8fa792b5c8fa504588
Aziz Syed <aziz.syed@nike.com>
2017-08-20 08:50:04 -0700

    Make presentable

--- app/global-styles.js
+++ app/global-styles.js
@@ -6,17 +6,17 @@ injectGlobal`
   body {
     height: 100%;
     width: 100%;
-    min-height: 400px;
-    min-width: 400px;
   }
 
   body {
     font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
-    background: linear-gradient( 0deg, #022c70, black );
+    background-image: linear-gradient( 135deg, #97ABFF 10%, #123597 100%);
+    // background-image: linear-gradient( 135deg, #3B2667 10%, #BC78EC 100%);
+    // background-image: linear-gradient( 135deg, #52E5E7 10%, #130CB7 100%);
+    // background: linear-gradient( 0deg, #022c70, black );
     // background-image: linear-gradient(to top, rgba(91,115,125,1) 0%, rgba(2,44,112,1) 50%, rgba(0,0,0,1) 100%);
     // background-image: radial-gradient(ellipse farthest-corner at center bottom, rgba(255,255,255,1) 0%, rgba(245,252,43,1) 33%, rgba(86,86,160,1) 66.5%, rgba(0,0,0,1) 100%);
     color: grey;
-    min-width: 400px;
     display: flex;
   }
 
@@ -29,51 +29,10 @@ injectGlobal`
     min-width: 100vw;
   }
 
-  #sun-svg {
-  }
-
   #hero-svg {
-    width: 60vw;
+    width: 70vw;
+    max-width: 1000px;
     opacity: 0;
     fill: transparent;
-  }
-
-  #hero {
-    position: relative;
-  }
-
-  @media (min-width: 480px) {
-    #sun-svg {
-      // transform: translate(-50%, 35%);
-      // width: 480px;
-    }
-  }
-
-  @media (min-width: 640px) {
-    #sun-svg {
-      // transform: translate(-50%, 40%);
-      // width: 640px;
-    }
-  }
-
-  @media (min-width: 800px) {
-    #sun-svg {
-      // transform: translate(-50%, 44%);
-      // width: 800px;
-    }
-  }
-
-  @media (min-width: 1024px) {
-    #sun-svg {
-      // transform: translate(-50%, 50%);
-      // width: 1024px;
-    }
-  }
-
-  @media (min-width: 1200px) {
-    #sun-svg {
-      // transform: translate(-50%, 60%);
-      // width: 1200px;
-    }
   }
 `;
